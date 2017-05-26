8decaa99ec55749b21bd444ed419b2f35066bf80
CoderSherlock <haopengzhan@gmail.com>
2017-05-26 17:14:26 -0400

    B: remove include in Config

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 sudo xl destroy Mini-OS
 make clean
-make LWIPDIR=/home/moslab/Github/opt-mini-os/lwip-2.0.2 XEN_ROOT=/home/moslab/Github/xen
+make LWIPDIR=/home/moslab/Github/opt-mini-os/lwip-2.0.2
 sudo xl create -c domain_config
